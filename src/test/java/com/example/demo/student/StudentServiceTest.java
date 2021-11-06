fd206cbd66350342459968e7f65cae744be38e62
rphlmnbt <rphlmnbt@gmail.com>
2021-11-06 21:25:52 +0800

    Set up Mockito.

--- src/test/java/com/example/demo/student/StudentServiceTest.java
+++ src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -1,21 +1,34 @@
 package com.example.demo.student;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 class StudentServiceTest {
 
+    @Mock
     private StudentRepository studentRepository;
+    private AutoCloseable autoCloseable;
     private StudentService underTest;
 
     @BeforeEach
     void setUp() {
+        autoCloseable = MockitoAnnotations.openMocks(this);
         underTest = new StudentService(studentRepository);
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        autoCloseable.close();
     }
 
     @Test
     void canGetAllStudents() {
+
     }
 
     @Test
