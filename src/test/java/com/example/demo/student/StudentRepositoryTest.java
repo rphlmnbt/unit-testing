a8f87fe2d352415148f790ad3f4e8c72241247af
rphlmnbt <rphlmnbt@gmail.com>
2021-11-06 21:03:51 +0800

    Annotated Student Repository Test as a Data JPA Test.

--- src/test/java/com/example/demo/student/StudentRepositoryTest.java
+++ src/test/java/com/example/demo/student/StudentRepositoryTest.java
@@ -2,9 +2,11 @@ package com.example.demo.student;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@DataJpaTest
 class StudentRepositoryTest {
 
     @Autowired
